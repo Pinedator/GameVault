3e97ae820c4f9ad76a867639514bd4290465190a
Sergio Pineda <sergio.pineda@alu.ceacfp.es>
2026-05-12 00:25:04 +0200

    fix : congirgure CORS for production

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,7 +11,10 @@ dotenv.config({ path: path.resolve(__dirname, "../.env") });
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://game-vault-ashy-six.vercel.app", "http://localhost:5173"],
+  credentials: true,
+}));
 app.use(express.json());
 
 app.use("/api/games", gamesRoutes);
